597c66077ed38a03b8a3b92b705f427622549800
yaakov <yaakov.sahale@gmail.com>
2021-08-05 18:54:28 +0300

    abc

---
+++ main.js
@@ -0,0 +1,267 @@
+
+//                     1
+
+
+// var num1 = Number(prompt("enter the first number"))
+// var num2 = Number(prompt("enter the seconed number"))
+
+// console.log({num1},{num2});
+
+// if(num1+num2 >= 13){
+//     console.log("bar-mitzva");
+// }
+// else if(num1+num2 == 12){
+//     console.log("bat-mitzva");
+// }
+// else{
+//     console.log("kid");
+// }
+
+
+
+
+//                     2
+
+
+// var counter = 0, num = Number(prompt("enter a number"))
+
+
+// while(counter < 100){
+//     console.log("yes");
+//     counter++
+// }
+
+// counter = 0
+
+// while(counter < num){
+//     console.log("no")
+//     counter++
+// }
+
+
+
+
+
+//                        3
+
+// for(var i=0; i<1000 ; i++ ){
+//     console.log("javascript is not JAVA");
+// }
+
+
+
+//                         4
+
+// var userName = prompt("enter a name")
+// var num = Number(prompt("enter a number"))
+
+// for(var i=0 ; i<num ; i++){
+//     console.log(userName);
+// }
+
+
+
+
+//                          5
+
+// var userName = prompt("enter your name")
+// var email = prompt("enter your email")
+// var userAge = Number(prompt("enter your age"))
+
+// console.log({userAge},{userAge},{email});
+
+// var numPrint = Number(prompt("enter a number"))
+
+// for(var i=0; userAge>18 && i<numPrint; i++){
+//     console.log({userAge},{userAge},{email});
+// }
+
+
+
+
+//                           6
+
+// var num = Number(prompt("enter a number"))
+// var myName = "yaakov"
+
+// for(var i=0; i<num;i++)
+// {    var userAge = prompt("enter your name")
+//     if(userAge == myName){
+//         console.log("hi boss "+myName);
+//     }
+//     else{
+//         console.log("no entry");
+//     }
+// }
+
+
+
+//                             7
+
+
+// var num1 = Number(prompt("enter the first number"))
+// var num2 = Number(prompt("enter the second number"))
+
+// for(var i=num1; i>0; i--){
+//     for(var j=num2; j>0; j--){
+//         console.log(i*j);
+//         console.log(i,j);
+//     }
+// }
+
+
+
+//                                   8-9
+
+// var myArry=[1,2,3,4,5,]
+// for(var i=0; i<myArry.length; i++){
+//     console.log(myArry[i]);
+// }
+
+
+
+//                               10
+
+
+// var myArry=[10,2,39,41,52,18,88,14]
+// var i = 0
+
+// while(i<myArry.length-1){
+//     if(myArry[i]%2==0){
+//         console.log(myArry[i]);
+//     }
+//     i++
+// }
+
+
+
+
+//                                     11
+
+// var myArry = []
+// for(var i=0; i<5; i++){
+//     myArry[i]=prompt("enter a number")
+// }
+// console.log(myArry);
+
+
+
+
+//                                            12
+
+// var myArry=[1,2,39,41,52,18,88,14]
+// var i = 0
+
+// while(i<myArry.length-1){
+//     if(myArry[i]%2!=0){
+//         console.log(myArry[i]);
+//     }
+//     i++
+// }
+
+
+
+
+                                    13
+
+// var myArry=[1,"qwer",4,"fg",45,698,"sdc","glhky",45]
+// var i = 0
+
+// while(i<myArry.length){
+//     if(isNaN(myArry[i])){
+//         myArry[i]="not_number"
+//     }
+//     i++
+// }
+
+// console.log(myArry);
+
+
+
+
+
+
+//                                  14
+
+
+// var myArry =[]
+// var i = 0, num
+
+// while(i<10){
+//     num=prompt("enter an even number")
+//     if(num%2==0){
+//         myArry.push(num)
+//         i++
+//     }
+// }
+// console.log(myArry);
+
+
+
+//                                        15
+
+
+// var widthRac = Number(prompt("enter a width for the rectangular"))
+// var heightRac = Number(prompt("enter a height for the rectangular"))
+
+// var area = widthRac * heightRac
+// var hekef = 2*widthRac + 2*heightRac
+
+// console.log({area},{hekef});
+
+
+
+
+
+                                            
+
+// var num1 = Number(prompt("enter the first number"));
+// var num2 = Number(prompt("enter the seconed number"));
+// var num3 = Number(prompt("enter the third number"));
+
+// if(( num1 > num2 ) && (num2 > num3)){
+//     console.log("down order");
+// }
+
+// else if(( num3 > num2 ) && (num2 > num1)){
+//     console.log("up order ");
+// }
+
+// else {
+//     console.log("no order");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+                               17
+
+// var num = Number(prompt("enter a number"))
+// var temp = 0
+
+// console.log(num);
+
+// while(num){
+//     temp = num%10
+//     num = parseInt(num/10)
+//     console.log(temp);
+// }
+
+
+
+
+
+
+
+
+
+
+
